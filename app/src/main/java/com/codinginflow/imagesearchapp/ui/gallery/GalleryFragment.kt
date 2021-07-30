5ff4469ee3a40359b2321fdd79f46d63cb0ff004
grizz03 <sawilson1110@yahoo.com>
2021-07-30 12:22:29 -0400

    connecting adapter to fragment and observing live data

--- app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.codinginflow.imagesearchapp.R
+import com.codinginflow.imagesearchapp.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 // Todo: Paging
@@ -13,12 +15,28 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
     private val viewModel by viewModels<GalleryViewModel>()
 
+    private var _binding: FragmentGalleryBinding? = null
+    private val binding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentGalleryBinding.bind(view)
 
-        viewModel.photos.observe(viewLifecycleOwner) {
+        val adapter = UnsplashPhotoAdapter()
 
+        binding.apply {
+            recyclerView.setHasFixedSize(true)
+            recyclerView.adapter = adapter
         }
 
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
