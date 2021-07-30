f7203aed585016da3f1ea36aca168bd59624a7ef
grizz03 <sawilson1110@yahoo.com>
2021-07-30 11:18:02 -0400

    Hilt Application created and gave name in manifest so it can be used

---
+++ app/src/main/java/com/codinginflow/imagesearchapp/ImageSearchApplication.kt
@@ -0,0 +1,10 @@
+package com.codinginflow.imagesearchapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+// give manifest the name of this application so it can be used.
+@HiltAndroidApp
+class ImageSearchApplication: Application() {
+}
