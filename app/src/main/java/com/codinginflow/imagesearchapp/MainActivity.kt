aec57b3c5598d02b6b794b93295bed35beabdbe0
grizz03 <sawilson1110@yahoo.com>
2021-07-30 11:17:14 -0400

    Created android entry point for dagger

--- app/src/main/java/com/codinginflow/imagesearchapp/MainActivity.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/MainActivity.kt
@@ -3,10 +3,10 @@ package com.codinginflow.imagesearchapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.codinginflow.imagesearchapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-//    private val accessKey = "84OanFXwFzt4bChmCYznpPv2Ci5IKC4MyPsZqtSeSW4"
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
